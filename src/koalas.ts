bd88dfd594fb8328f2b10cbeab373fe4f407b222
vanthang559 <vanthang23032001@gmail.com>
2024-11-14 14:35:44 +0700

    feat: change to koala graph

--- src/koalas.ts
+++ src/koalas.ts
@@ -1,135 +1,65 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   Approval as ApprovalEvent,
   ApprovalForAll as ApprovalForAllEvent,
   Mint as MintEvent,
+  OwnershipTransferred,
   OwnershipTransferred as OwnershipTransferredEvent,
   Paused as PausedEvent,
+  Transfer,
   Transfer as TransferEvent,
   Unpaused as UnpausedEvent,
   WhitelistMint as WhitelistMintEvent
 } from "../generated/Koalas/Koalas"
 import {
-  Approval,
-  ApprovalForAll,
   Mint,
-  OwnershipTransferred,
-  Paused,
-  Transfer,
-  Unpaused,
-  WhitelistMint
+  WhitelistMint,
 } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleMint(event: MintEvent): void {
-  let entity = new Mint(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.to = event.params.to
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Mint.load(getHexStringFromEventParams(event.params.amount));
+  if (!entity) {
+    entity = new Mint(getHexStringFromEventParams(event.params.amount));
+    entity.to = event.params.to;
+    entity.amount = event.params.amount;
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+  }
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+}
 
-  entity.save()
+export function handlePaused(event: PausedEvent): void {
 }
 
 export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleApproval(event: ApprovalEvent): void {
+}
 
-  entity.save()
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
 }
 
 export function handleWhitelistMint(event: WhitelistMintEvent): void {
-  let entity = new WhitelistMint(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.to = event.params.to
-  entity.amount = event.params.amount
+  let entity = WhitelistMint.load(getHexStringFromEventParams(event.params.amount));
+  if (!entity) {
+    entity = new WhitelistMint(getHexStringFromEventParams(event.params.amount));
+    entity.to = event.params.to;
+    entity.amount = event.params.amount;
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+  }
+  entity.save();
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+function getHexStringFromEventParams(tokenId: BigInt): string {
+  return tokenId.toHexString();
 }
